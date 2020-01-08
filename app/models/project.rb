1320e9873eb70814e213e59206945ac650835ee6
parthbpatel <parthtech3010@gmail.com>
2020-01-08 18:06:11 +0530

    Add dependent: :delete_all in project model for messages.

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,5 @@
 class Project < ApplicationRecord
   validates :name, presence: true
   belongs_to :user
-  has_many :messages, dependent: :destroy
+  has_many :messages, dependent: :delete_all
 end
