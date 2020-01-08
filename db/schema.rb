a9afe06309749f6d0415552202fa4ecd72f2da78
parthbpatel <parthtech3010@gmail.com>
2020-01-08 17:17:42 +0530

    Add blog creation feature in devcamp appliaction, create separate view for all blogs, Reader can click on title link and can read full blog separately, add css for the same.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_08_052740) do
+ActiveRecord::Schema.define(version: 2020_01_08_093227) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "blogs", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "messages", force: :cascade do |t|
     t.text "content"
